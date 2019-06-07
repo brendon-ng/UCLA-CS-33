9035b5002ec46a12705fd93eeeec8c01f257899c
brendon-ng <brendonn8@gmail.com>
2019-06-06 23:10:56 -0700

    1.5

--- ParallelLab/1.2.c
+++ ParallelLab/1.2.c
@@ -19,13 +19,12 @@ void work_it_par(long *old, long *new) {
   long we_need_the_func_var = we_need_the_func();
   long sum = 0.0;
   int DIM2 = DIM*DIM;
-  int iDIM2, jDIM, index;
-  iDIM2 = DIM2;
-
+  int iDIM2 = DIM2;
+  
   for (i=1; i<DIM-1; i++) {
-    jDIM = DIM;
+    int jDIM = DIM;
     for (j=1; j<DIM-1; j++) {
-      index = iDIM2 + jDIM;
+      int index = iDIM2 + jDIM;
       for (k=1; k<DIM-1; k++) {
         sum+= old[index+k] * we_need_the_func_var/gimmie_the_func_var;
       }
@@ -38,31 +37,20 @@ void work_it_par(long *old, long *new) {
 
   printf("AGGR:%ld\n",aggregate);
 
-  int  uDIM2, vDIM;
-  iDIM2 = DIM2;
-  int negDIM = -1*DIM;
-  int negDIM2 = negDIM*DIM;
   for (i=1; i<DIM-1; i++) {
-    jDIM = DIM;
     for (j=1; j<DIM-1; j++) {
       for (k=1; k<DIM-1; k++) {
-        int sum = 0;
-	uDIM2 = negDIM2;
+        new[i*DIM*DIM+j*DIM+k]=0;
         for (u=-1; u<=1; u++) {
-	  vDIM = negDIM;
           for (v=-1; v<=1; v++) {
             for (w=-1; w<=1; w++) {
-               sum+=old[iDIM2+uDIM2+jDIM+vDIM+(k+w)];
+               new[i*DIM*DIM+j*DIM+k]+=old[(i+u)*DIM*DIM+(j+v)*DIM+(k+w)];
             }
-	    vDIM += DIM;
           }
-	  uDIM2 += DIM2;
-        }	
-        new[iDIM2+jDIM+k] = sum / 27;
+        }
+        new[i*DIM*DIM+j*DIM+k]/=27;
       }
-      jDIM += DIM;
     }
-    iDIM2 += DIM2;
   }
 
   for (i=1; i<DIM-1; i++) {
--- ParallelLab/original.c
+++ ParallelLab/original.c
@@ -1,8 +1,8 @@
 //OpenMP version.  Edit and submit only this file.
 /* Enter your details below
- * Name : Brendon Ng
- * UCLA ID : 304-925-492
- * Email : brendonn8@ucla.edu
+ * Name :
+ * UCLA ID : 
+ * Email :
  */
 
 #include <stdlib.h>
@@ -14,55 +14,34 @@
 void work_it_par(long *old, long *new) {
   int i, j, k;
   int u, v, w;
+  long compute_it;
   long aggregate=1.0;
-  long gimmie_the_func_var = gimmie_the_func();
-  long we_need_the_func_var = we_need_the_func();
-  long sum = 0.0;
-  int DIM2 = DIM*DIM;
-  int iDIM2, jDIM, index;
-  iDIM2 = DIM2;
 
   for (i=1; i<DIM-1; i++) {
-    jDIM = DIM;
     for (j=1; j<DIM-1; j++) {
-      index = iDIM2 + jDIM;
       for (k=1; k<DIM-1; k++) {
-        sum+= old[index+k] * we_need_the_func_var/gimmie_the_func_var;
+        compute_it = old[i*DIM*DIM+j*DIM+k] * we_need_the_func();
+        aggregate+= compute_it / gimmie_the_func();
       }
-      jDIM += DIM;
     }
-    iDIM2 += DIM2;
   }
- 
-  aggregate += sum;
 
   printf("AGGR:%ld\n",aggregate);
 
-  int  uDIM2, vDIM;
-  iDIM2 = DIM2;
-  int negDIM = -1*DIM;
-  int negDIM2 = negDIM*DIM;
   for (i=1; i<DIM-1; i++) {
-    jDIM = DIM;
     for (j=1; j<DIM-1; j++) {
       for (k=1; k<DIM-1; k++) {
-        int sum = 0;
-	uDIM2 = negDIM2;
+        new[i*DIM*DIM+j*DIM+k]=0;
         for (u=-1; u<=1; u++) {
-	  vDIM = negDIM;
           for (v=-1; v<=1; v++) {
             for (w=-1; w<=1; w++) {
-               sum+=old[iDIM2+uDIM2+jDIM+vDIM+(k+w)];
+               new[i*DIM*DIM+j*DIM+k]+=old[(i+u)*DIM*DIM+(j+v)*DIM+(k+w)];
             }
-	    vDIM += DIM;
           }
-	  uDIM2 += DIM2;
-        }	
-        new[iDIM2+jDIM+k] = sum / 27;
+        }
+        new[i*DIM*DIM+j*DIM+k]/=27;
       }
-      jDIM += DIM;
     }
-    iDIM2 += DIM2;
   }
 
   for (i=1; i<DIM-1; i++) {
